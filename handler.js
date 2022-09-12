497c36175ec648b2b960c14cd54013d1ed345466
Rakib Ansary <rakibansary@gmail.com>
2022-09-12 22:53:26 +0600

    fix: incorrect permissions

--- handler.js
+++ handler.js
@@ -115,7 +115,6 @@ async function saveToS3Promise(
     Bucket: dwDestBucket,
     Key: `${dwOutputBucketPathPrefix}/${destKey}`,
     Body: Buffer.from(JSON.stringify(dynamodb)),
-    ACL: "bucket-owner-full-control",
   };
 
   console.log("Uploading to S3...", params);
