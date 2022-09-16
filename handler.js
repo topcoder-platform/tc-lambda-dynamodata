358ea5ce7d0869be992b6f62974bb436738815cf
Rakib Ansary <rakibansary@gmail.com>
2022-09-16 14:54:36 +0600

    feat: add cloudwatch metrics

--- handler.js
+++ handler.js
@@ -2,18 +2,16 @@ const { snakeCase } = require("change-case");
 const { DynamoDB } = require("aws-sdk");
 
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-
-// const {
-//   CloudWatchClient,
-//   PutMetricDataCommand,
-// } = require("@aws-sdk/client-cloudwatch");
+const {
+  CloudWatchClient,
+  PutMetricDataCommand,
+} = require("@aws-sdk/client-cloudwatch");
 
 const s3Client = new S3Client();
-
-// const cloudwatchClient = new CloudWatchClient({
-//   apiVersion: "2010-08-01",
-//   region: process.env.AWS_DEFAULT_REGION,
-// });
+const cloudwatchClient = new CloudWatchClient({
+  apiVersion: "2010-08-01",
+  region: process.env.AWS_DEFAULT_REGION,
+});
 
 const dwOutputBucket = process.env.DW_OUTPUT_BUCKET || "tc-dw-dev-dw-raw";
 const dwOutputBucketPathPrefix =
@@ -23,10 +21,11 @@ exports.processAndSave = function main(event, context) {
   console.log("Event", JSON.stringify(event));
 
   const tasks = [];
+  const tableName = eventSourceARN.split("/")[1];
 
   const data = event.Records.map(({ dynamodb, eventSourceARN }) => ({
     dynamodb: DynamoDB.Converter.unmarshall(dynamodb.NewImage),
-    tableName: eventSourceARN.split("/")[1],
+    tableName,
   }));
 
   for (const record of data) {
@@ -35,24 +34,24 @@ exports.processAndSave = function main(event, context) {
     );
   }
 
-  Promise.all(tasks)
-    .then(async () => {
-      //   const query = getCWQuery("Success", event.Records[0].s3.object.key);
-      //   const command = new PutMetricDataCommand(query);
+  Promise.all(tasks).then(async (results) => {
+    const successCount = results.filter((result) => result).length;
+    const failureCount = results.filter((result) => !result).length;
 
-      //   await cloudwatchClient.send(command);
+    if (successCount) {
+      const query = getCWQuery("Success", tableName, successCount);
+      const command = new PutMetricDataCommand(query);
+      await cloudwatchClient.send(command);
+    }
 
-      context.succeed();
-    })
-    .catch(async (err) => {
-      console.error(`Failed with ${err}`);
-      //   const query = getCWQuery("Failure", event.Records[0].s3.object.key);
-      //   const command = new PutMetricDataCommand(query);
+    if (failureCount) {
+      const query = getCWQuery("Failure", tableName, successCount);
+      const command = new PutMetricDataCommand(query);
+      await cloudwatchClient.send(command);
+    }
 
-      //   await cloudwatchClient.send(command);
-
-      context.fail();
-    });
+    context.succeed();
+  });
 };
 
 const getPartitionKey = () => {
@@ -65,19 +64,19 @@ const getPartitionKey = () => {
   return `${year}/${month}/${day}`;
 };
 
-const getCWQuery = (metricName, key) => {
+const getCWQuery = (metricName, tableName, count) => {
   return {
     MetricData: [
       {
         MetricName: metricName,
         Dimensions: [
           {
-            Name: "Process Auth0 Logs",
-            Value: key,
+            Name: `Stream to data warehouse`,
+            Value: tableName,
           },
         ],
-        Unit: "None",
-        Value: 1.0,
+        Unit: "Count",
+        Value: count,
         Timestamp: new Date(),
       },
     ],
@@ -106,18 +105,23 @@ async function saveToS3Promise(
   dwDestBucket,
   dwOutputBucketPathPrefix
 ) {
-  tableName = snakeCase(tableName);
-  const destKey = `${tableName}/${getPartitionKey()}/${
-    dynamodb[getPrimaryKey(tableName)]
-  }.json`;
+  try {
+    tableName = snakeCase(tableName);
+    const destKey = `${tableName}/${getPartitionKey()}/${
+      dynamodb[getPrimaryKey(tableName)]
+    }.json`;
 
-  const params = {
-    Bucket: dwDestBucket,
-    Key: `${dwOutputBucketPathPrefix}/${destKey}`,
-    Body: Buffer.from(JSON.stringify(dynamodb)),
-  };
+    const params = {
+      Bucket: dwDestBucket,
+      Key: `${dwOutputBucketPathPrefix}/${destKey}`,
+      Body: Buffer.from(JSON.stringify(dynamodb)),
+    };
 
-  console.log("Uploading to S3...", params);
-  await s3Client.send(new PutObjectCommand(params));
-  console.log("Successfully uploaded to S3");
+    console.log("Uploading to S3...", params);
+    await s3Client.send(new PutObjectCommand(params));
+    console.log("Successfully uploaded to S3");
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
