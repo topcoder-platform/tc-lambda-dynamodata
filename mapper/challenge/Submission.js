6315faefcee925b8db1e4319f8d9321847f08d7d
Hamid Tavakoli <hamid@seodapop.com>
2022-12-06 01:42:47 -0600

    Fix the date time issue for challenge and submission

--- mapper/challenge/Submission.js
+++ mapper/challenge/Submission.js
@@ -1,3 +1,4 @@
+const moment = require("moment");
 const mapSubmission = (submission) => {
   if (typeof submission.challengeId === "number") {
     // This is a legacy submission, we need to convert the challengeId to a string
@@ -7,6 +8,8 @@ const mapSubmission = (submission) => {
     // This is a legacy submission, we need to convert the submissionPhaseId to a string
     submission.submissionPhaseId = String(submission.submissionPhaseId);
   }
+  submission.updated = +moment(submission.updated).format('x');
+  submission.created = +moment(submission.created).format('x');
   return submission;
 };
 
